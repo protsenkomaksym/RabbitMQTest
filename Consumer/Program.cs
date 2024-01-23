d707d52f110a3f24639df4bb56f633536af19e8f
protsenkomaksym <maksim20032@hotmail.com>
2024-01-23 19:15:24 -0300

    Update: Sender and Receiver

---
+++ Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.HostName = "localhost";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel model = connection.CreateModel();
+
+model.QueueDeclare(queue: "testQueue",
+                 durable: false,
+                 exclusive: false,
+                 autoDelete: false,
+                 arguments: null);
+
+// Read queue body
+var consumer = new EventingBasicConsumer(model);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+model.BasicConsume(queue: "testQueue",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.ReadKey();
--- RabbitMQTest/Program.cs
+++ RabbitMQTest/Program.cs
@@ -1,84 +1,38 @@
 ﻿using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
 using System.Text;
-using System.Threading.Channels;
 
-bool sender = false;
-bool receiver = true;
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.HostName = "localhost";
 
-if (sender)
+IConnection connection = connectionFactory.CreateConnection();
+IModel model = connection.CreateModel();
+
+string exchange = "testExchange";
+string routingKey = "testKey";
+string queue = "testQueue";
+
+// Create exchange
+model.ExchangeDeclare(exchange, ExchangeType.Direct);
+
+// Create queue
+model.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+// Bind rounting queue-key
+model.QueueBind(queue, exchange, routingKey, arguments: null);
+
+// Send messages to the queue
+for (int i = 0; i <= 5; i++)
 {
-    ConnectionFactory connectionFactory = new ConnectionFactory();
-    connectionFactory.HostName = "localhost";
-
-    IConnection connection = connectionFactory.CreateConnection();
-    IModel model = connection.CreateModel();
-
-    string exchange = "testE";
-    string routingKey = "testK";
-    string queue = "testQ";
-
-    // Create excchange
-    model.ExchangeDeclare(exchange, ExchangeType.Direct);
-
-    // Create queue
-    model.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
-
-    // Bind queue-key
-    model.QueueBind(queue, exchange, routingKey, arguments: null);
-
-    // Messege body
-    string msg = "Hello";
+    string msg = $"Msg #{i}";
     model.BasicPublish(exchange, routingKey, basicProperties: null, Encoding.UTF8.GetBytes(msg));
+    Console.WriteLine(msg);
 
-    // Close connection
-    model.Close();
-    connection.Close();
+    // Wait 2sec
+    Thread.Sleep(2000);
 }
 
-// receiver
-if (receiver)
-{
-    ConnectionFactory connectionFactory = new ConnectionFactory();
-    connectionFactory.HostName = "localhost";
-
-    IConnection connection = connectionFactory.CreateConnection();
-    IModel model = connection.CreateModel();
-
-    string exchange = "testE";
-    string routingKey = "testK";
-    string queue = "testQ";
-
-    model.QueueDeclare(queue: "testQ",
-                     durable: false,
-                     exclusive: false,
-                     autoDelete: false,
-                     arguments: null);
-    // Create excchange
-    ///model.ExchangeDeclare(exchange, ExchangeType.Direct);
-
-    // Create queue
-    //model.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
-
-    // Bind queue-key
-    //model.QueueBind(queue, exchange, routingKey, arguments: null);
-
-    // Read queue body
-    var consumer = new EventingBasicConsumer(model);
-    consumer.Received += (model, ea) =>
-    {
-        var body = ea.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
-        Console.WriteLine($" [x] Received {message}");
-    };
-
-    model.BasicConsume(queue: "testQ",
-                         autoAck: true,
-                         consumer: consumer);
-
-    // Close connection
-    //model.Close();
-    //connection.Close();
-}
+// Close connection
+model.Close();
+connection.Close();
 
 Console.ReadKey();
